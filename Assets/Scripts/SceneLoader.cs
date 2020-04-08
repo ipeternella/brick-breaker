ae851130925dc71c655d53a5f4b78c049607b1a9
Igor Grillo Peternella <igor.feq@gmail.com>
2020-04-08 16:57:10 -0300

    Added SceneLoader class to handle scene changing.

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+   // loads next scene based on the scene ordering defined on Unity > build settings
+   public void LoadNextScene()
+   {
+      int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+      SceneManager.LoadScene(currentSceneIndex + 1);
+   }
+
+   // always the 0 indexed scene
+   public void LoadStartScene()
+   {
+      SceneManager.LoadScene(0);
+   }
+
+   public void Quit()
+   {
+      Application.Quit();
+   }
+}
