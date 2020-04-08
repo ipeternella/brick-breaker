f14e2e09be3755ddd06dee4a2ef22cf067cf2fd2
Igor Grillo Peternella <igor.feq@gmail.com>
2020-04-08 16:57:54 -0300

    Added Paddle class with paddle logic.

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Unity units of the WIDTH of the screen (e.g. 16)
+    [SerializeField]
+    public float screenWidthUnits = 16;
+    private const float PADDLE_FIXED_RELATIVE_POS_Y = 0.38f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float startPosX = ConvertPixelToRelativePosition(screenWidthUnits / 2, Screen.width);
+        transform.position = GetUpdatedPaddlePosition(startPosX);
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        var relativePosX = ConvertPixelToRelativePosition(pixelPosition: Input.mousePosition.x, Screen.width);
+        transform.position = GetUpdatedPaddlePosition(relativePosX);
+    }
+
+    public Vector2 GetUpdatedPaddlePosition(float relativePosX)
+    {
+        Vector2 newPaddlePosition = new Vector2(relativePosX, PADDLE_FIXED_RELATIVE_POS_Y);
+        return newPaddlePosition;
+    }
+    
+    public float ConvertPixelToRelativePosition(float pixelPosition, int screenWidth)
+    { 
+        var relativePosition = pixelPosition/screenWidth * screenWidthUnits;
+        return relativePosition;
+    }
+
+}
