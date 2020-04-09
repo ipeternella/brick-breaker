e29b162c1851d5c817861f4c69164d91c8db7dc7
Igor Grillo Peternella <igor.feq@gmail.com>
2020-04-09 14:56:44 -0300

    Created block class. Added collision destroyer logic.

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    { 
+        Destroy(this.gameObject);
+    }
+}
