1101ca7133a25e6753f3db5e9ae4d0e21b5d7f77
Igor Grillo Peternella <igor.feq@gmail.com>
2020-04-08 22:56:14 -0300

    Added code to make ball stick to the paddle before launching it.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    [SerializeField] 
+    public Paddle paddle;  // bound to the Paddle object on the UI
+    public Vector2 distanceToPaddle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        var ballPosition = transform.position;
+        var paddlePosition = paddle.transform.position;
+
+        distanceToPaddle = ballPosition - paddlePosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var paddlePosition = paddle.transform.position;
+        transform.position = new Vector2(distanceToPaddle.x + paddlePosition.x, distanceToPaddle.y + paddlePosition.y);
+    }
+}
