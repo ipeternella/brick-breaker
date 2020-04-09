142dbb37aae5c4af0829f3c6c992e12e85b83a02
Igor Grillo Peternella <igor.feq@gmail.com>
2020-04-09 19:50:39 -0300

    Added smash sound to broken blocks.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] public AudioClip destroyedBlockSound;
+    [SerializeField] public float soundVolume = 0.05f;
+    
     private void OnCollisionEnter2D(Collision2D other)
-    { 
+    {
+        AudioSource.PlayClipAtPoint(destroyedBlockSound, Camera.current.transform.position, soundVolume);
         Destroy(this.gameObject);
     }
 }
