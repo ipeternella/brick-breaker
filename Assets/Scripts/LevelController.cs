b8c5260fc319508e0fca2655994ce7409456ad88
Igor Grillo Peternella <igor.feq@gmail.com>
2020-04-10 12:46:40 -0300

    Added Level controller to count the amount of blocks left on the level

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    // UI elements
+    [SerializeField] int blocksCounter;
+
+    // state
+    private SceneLoader _sceneLoader;
+    
+    private void Start()
+    {
+        _sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public void IncrementBlocksCounter()
+    {
+        blocksCounter++;
+    }
+    
+    public void DecrementBlocksCounter()
+    {
+        blocksCounter--;
+
+        if (blocksCounter <= 0)
+        {
+            _sceneLoader.LoadNextScene();
+        }
+    }
+    
+}
