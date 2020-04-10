8065b1f94bb3f4189b3ee6bed56a24d63a9fb50b
Igor Grillo Peternella <igor.feq@gmail.com>
2020-04-10 17:07:10 -0300

    Bound score text to game state's score for level 3.

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,21 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
     // state
+    [SerializeField] private TextMeshProUGUI playerScoreText;
     [SerializeField] private int playerScore = 0;
 
-    
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerScoreText.text = playerScore.ToString();
     }
 
+    /**
+     * Updates player score with given points and also updates the UI score.
+     */
     public void AddToPlayerScore(int points)
     {
         playerScore += points;
+        playerScoreText.text = playerScore.ToString();
     }
 }
