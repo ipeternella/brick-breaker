637f85b3869831ddbab3faf86213e43fae518c9f
Igor Grillo Peternella <igor.feq@gmail.com>
2020-04-10 12:47:19 -0300

    Added logic for blocks to update the level controller upon collision.

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -8,9 +8,37 @@ public class Block : MonoBehaviour
     [SerializeField] public AudioClip destroyedBlockSound;
     [SerializeField] public float soundVolume = 0.05f;
     
+    // state
+    private LevelController levelController;
+
+    void Start()
+    { 
+        // selects other game object without SCENE binding: programatically via API
+        levelController = FindObjectOfType<LevelController>();
+
+        // every block on the scene, increments it by one 
+        levelController.IncrementBlocksCounter();
+    }
+    
+    /**
+     * Destroys the block upon a collision. 
+     */
     private void OnCollisionEnter2D(Collision2D other)
     {
+        DestroyItself();
+    }
+
+    /**
+     * Upon a collision, the block must be destroyed. Once a block is destroyed, the blocks counter
+     * of the level controller must be decremented.
+     */    
+    private void DestroyItself()
+    {
+        // plays destroyed block sound 
         AudioSource.PlayClipAtPoint(destroyedBlockSound, Camera.current.transform.position, soundVolume);
         Destroy(this.gameObject);
+
+        // increments destroyed blocks of the level
+        levelController.DecrementBlocksCounter();
     }
 }
