a03e2e2327d2b2d0a38b3331c5ec0198a2e363df
Igor Grillo Peternella <igor.feq@gmail.com>
2020-04-10 15:40:54 -0300

    Added GameConfig game object to control game speed.

---
+++ Assets/GameConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConfig : MonoBehaviour
+{
+    [Range(0.5f, 2f)] [SerializeField] private float gameSpeed = 0.8f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+}
