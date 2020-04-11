d345d5f09aa161d456a9d21c73dc19e421e67036
Igor Grillo Peternella <igor.feq@gmail.com>
2020-04-10 21:20:29 -0300

    Addled singleton pattern to GameState.cs

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -5,10 +5,30 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
+    
     // state
     [SerializeField] private TextMeshProUGUI playerScoreText;
     [SerializeField] private int playerScore = 0;
+    
+    // Singleton implementation
+    private static GameState _instance;
+    public static GameState Instance => _instance;
 
+    // Initialization hook before start
+    private void Awake() 
+    { 
+        // this is not the first instance so destroy it!
+        if (_instance != null && _instance != this)
+        { 
+            Destroy(this.gameObject);
+            return;
+        }
+        
+        // first instance should be kept and do NOT destroy it on load
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
