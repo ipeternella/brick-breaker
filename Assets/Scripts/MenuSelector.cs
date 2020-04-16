c8e550c1ec9dfbfa75b5dd3930b288be233758b2
Igor Grillo Peternella <igor.feq@gmail.com>
2020-04-16 16:03:48 -0300

    Start scene menu now inherits from base vertical menu class.

--- Assets/Scripts/MenuSelector.cs
+++ Assets/Scripts/MenuSelector.cs
@@ -1,91 +1,51 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SocialPlatforms;
 using UnityEngine.UIElements;
 
-public class MenuSelector : MonoBehaviour
+public class MenuSelector : VerticalMenuSelector
 {
-    
-    // configuration
-    [SerializeField] public GameObject[] menuOptions;
-    [Range(0, 100)][SerializeField] public int marginLeft = 100;
-
     // status
-    private int _selectedMenuIndex = 0;
-
-    // Start is called before the first frame update
+    private readonly string MENU_OPTION_START = "MenuOptionStart";
+    private readonly string MENU_OPTION_OPTIONS = "MenuOptionOptions";
+    
+    /**
+     * Before first frame update. 
+     */
     void Start()
     {
-        transform.position = GetMenuSelectorPosition(this._selectedMenuIndex);
+        transform.position = GetMenuSelectorPosition();
     }
 
-    private Vector2 GetMenuSelectorPosition(int menuOptionIndex)
-    {
-        GameObject menuOption = this.menuOptions[menuOptionIndex];
-        
-        var selectorY = menuOption.transform.position.y;
-        Vector2 selectorPosition = new Vector2(transform.position.x, selectorY);
-
-        return selectorPosition;
-    }
-
-    // Update is called once per frame
+    /**
+     * Update per frame.
+     */
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            HandleVerticalMovement(KeyCode.DownArrow);
-        } 
-        else if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            HandleVerticalMovement(KeyCode.UpArrow);
-        }
-        else if (Input.GetKeyDown(KeyCode.Return))
-        {
-            HandleReturn();
-        }
-
+        // invokes base class up/down arrows handling
+        this.HandleUpDownArrowPresses();
+        
+        // enter case handling
+        if (Input.GetKeyDown(KeyCode.Return)) HandleReturn();
     }
-
+    
+    /**
+     * Handles ENTER pressing that allows the user to choose an option.
+     */
     private void HandleReturn()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            GameObject menuOption = this.menuOptions[_selectedMenuIndex];
+        GameObject currentMenu = this.GetCurrentMenu();
+
+        if (currentMenu.name == this.MENU_OPTION_START) 
+            this.sceneLoader.LoadSceneByName("Level1");
         
-            if (menuOption.name == "MenuOptionStart")
-            {
-                FindObjectOfType<SceneLoader>().LoadNextScene();
-                
-            } else if (menuOption.name == "MenuOptionOptions")
-            {
-                FindObjectOfType<SceneLoader>().Quit();
-            }
-            else
-            {
-                FindObjectOfType<SceneLoader>().Quit();
-            }
-        }
-    }
-
-    private void HandleVerticalMovement(KeyCode key)
-    {
-        var maxOptionIndex = this.menuOptions.Length - 1;
-
-        // updating selected menu index and safety clamping
-        if (key == KeyCode.DownArrow)
-        {
-            this._selectedMenuIndex++;
-        }
-        else if (key == KeyCode.UpArrow)
-        {
-            this._selectedMenuIndex--;
-        }
-        this._selectedMenuIndex = Mathf.Clamp(_selectedMenuIndex, 0, maxOptionIndex);
-
-        // updating position
-        transform.position = GetMenuSelectorPosition(this._selectedMenuIndex);
+        else if (currentMenu.name == this.MENU_OPTION_OPTIONS) 
+            this.sceneLoader.LoadSceneByName("OptionsMenu");
+        
+        else 
+            this.sceneLoader.Quit();
     }
 }
